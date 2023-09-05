d3eeef63789b19323e689afc609714ce6361d1fa
Xavier Mitault <xavier.mitault@epitech.eu>
2023-09-05 18:35:53 +0200

    Fix (Footer): error when clicking arrow

--- trombone/Components/Footer.tsx
+++ trombone/Components/Footer.tsx
@@ -1,70 +1,108 @@
 import Colors from '@app/Colors';
 import Icons from '@app/Icons';
 import Button from '@app/Components/Button';
-import { Animated, Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
-import { useEffect, useState } from 'react';
+import { Animated, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
+function  navigateName(navigation: any, name: string, key: string) {
+    const event = navigation.emit({
+      type: 'tabPress',
+      target: key,
+      canPreventDefault: true,
+    });
+
+    if (!event.defaultPrevented) {
+      // The `merge: true` option makes sure that the params inside the tab screen are preserved
+      navigation.navigate({ name: name, merge: true });
+    }
+}
 
 export function MyTabBar({ state, descriptors, navigation, position }: any) {
+  const goLeft = () => {
+      let route = undefined
+      if (state.index === 0) {
+          route = state.routes[state.routeNames.length - 1]
+      } else {
+          route = state.routes[state.index - 1]
+      }
+
+      navigateName(navigation, route.name, route.key)
+  }
+  const goRight = () => {
+      let route = undefined;
+      if (state.index === state.routeNames.length - 1) {
+          route = state.routes[0]
+      } else {
+          route = state.routes[state.index + 1]
+      }
+
+      navigateName(navigation, route.name, route.key)
+  }
+
   return (
-    <View style={{ flexDirection: 'row' }}>
-      {state.routes.map((route: any, index: any) => {
-        const { options } = descriptors[route.key];
-        const label =
-          options.tabBarLabel !== undefined
-            ? options.tabBarLabel
-            : options.title !== undefined
-            ? options.title
-            : route.name;
+    <View style={styles.container}>
+        <Button onPress={() => { goLeft() }} >
+            <Image source={Icons.arrowLeft} style={styles.arrowLeftIcon} />
+        </Button>
+        <View style={{ flexDirection: 'row' }}>
+          {state.routes.map((route: any, index: any) => {
+            const { options } = descriptors[route.key];
 
-        const isFocused = state.index === index;
+            const isFocused = state.index === index;
 
-        const onPress = () => {
-          const event = navigation.emit({
-            type: 'tabPress',
-            target: route.key,
-            canPreventDefault: true,
-          });
+            const onPress = () => {
+              navigateName(navigation, route.name, route.key)
+            };
 
-          if (!isFocused && !event.defaultPrevented) {
-            // The `merge: true` option makes sure that the params inside the tab screen are preserved
-            navigation.navigate({ name: route.name, merge: true });
-          }
-        };
+            const onLongPress = () => {
+              navigation.emit({
+                type: 'tabLongPress',
+                target: route.key,
+              });
+            };
 
-        const onLongPress = () => {
-          navigation.emit({
-            type: 'tabLongPress',
-            target: route.key,
-          });
-        };
-
-        const inputRange = state.routes.map((_: any, i: any) => i);
-        const opacity = position.interpolate({
-          inputRange,
-          outputRange: inputRange.map((i: any) => (i === index ? 1 : 0)),
-        });
-
-        return (
-          <TouchableOpacity
-            accessibilityRole="button"
-            accessibilityState={isFocused ? { selected: true } : {}}
-            accessibilityLabel={options.tabBarAccessibilityLabel}
-            testID={options.tabBarTestID}
-            onPress={onPress}
-            onLongPress={onLongPress}
-            style={{ flex: 1 }}
-          >
-            <Animated.Text style={{ opacity }}>
-              {label}
-            </Animated.Text>
-          </TouchableOpacity>
-        );
-      })}
+            return (
+              <TouchableOpacity
+                accessibilityRole="button"
+                accessibilityState={isFocused ? { selected: true } : {}}
+                accessibilityLabel={options.tabBarAccessibilityLabel}
+                testID={options.tabBarTestID}
+                onPress={onPress}
+                onLongPress={onLongPress}
+                key={index}
+              >
+                <Animated.View style={(isFocused ? styles.circlePlain : styles.circleEmpty)} />
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+        <Button onPress={() => { goRight() }} >
+            <Image source={Icons.arrowRight} style={styles.arrowRightIcon} />
+        </Button>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+    circlePlain: {
+        marginVertical: 10,
+        width: 20,
+        height: 20,
+        borderRadius: 50000,
+        borderWidth: 2,
+        borderColor: '#000',
+        backgroundColor: Colors.gradient1,
+        marginHorizontal: 5
+    },
+    circleEmpty: {
+        marginVertical: 10,
+        width: 20,
+        height: 20,
+        borderRadius: 50000,
+        borderWidth: 2,
+        borderColor: '#000',
+        backgroundColor: Colors.secondary,
+        marginHorizontal: 5
+    },
     container: {
         backgroundColor: Colors.secondary,
         elevation: 10,
