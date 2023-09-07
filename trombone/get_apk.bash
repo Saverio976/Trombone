32d50232191d954b08cbac7ce59742192d8da8f3
Xavier Mitault <xavier.mitault@epitech.eu>
2023-09-07 22:30:36 +0200

    Fix (CICD): building without values

--- trombone/get_apk.bash
+++ trombone/get_apk.bash
@@ -3,10 +3,14 @@
 set -e
 set -x
 
-cp ./.env.default ./.env
-sed -i "s/=XGROUPAUTHORIZATION/$XGROUPAUTHORIZATION/g" ./.env
-sed -i "s/=API_URL/$API_URL/g" ./.env
+if [ ! -f './.env' ]; then
+    cp ./.env.default ./.env
+    sed -i "s/=XGROUPAUTHORIZATION/=$XGROUPAUTHORIZATION/g" ./.env
+    sed -i "s/=API_URL/=$API_URL/g" ./.env
+fi
 
-cd android
+cd android || exit 1
+
 ./gradlew --no-daemon assembleRelease
+
 cp app/build/outputs/apk/release/app-release.apk ..
