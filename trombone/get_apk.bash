27ce80e8dbafcfc4f62c301f3e3a2e4b8617ce1e
Xavier Mitault <xavier.mitault@epitech.eu>
2023-09-07 17:53:12 +0200

    Fix (CICD): not building when epitech repo

--- trombone/get_apk.bash
+++ trombone/get_apk.bash
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cp ./.env.default ./.env
+sed -i "s/=XGROUPAUTHORIZATION/$XGROUPAUTHORIZATION/g" ./.env
+sed -i "s/=API_URL/$API_URL/g" ./.env
+
 cd android
-./gradlew assembleRelease
+./gradlew --no-daemon assembleRelease
 cp app/build/outputs/apk/release/app-release.apk ..
